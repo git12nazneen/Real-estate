475b222b2d2785ccbe0215255db38be2c31b0048
git12nazneen <lipinazneen65@gmail.com>
2024-04-16 13:14:38 +0600

    added profile

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -15,6 +15,5 @@ const firebaseConfig = {
   messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID ,
   appId:import.meta.env.VITE_APPID ,
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
